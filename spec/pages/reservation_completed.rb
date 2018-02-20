b6f18ac9d49f8d1887bf0399eb08e0956cd8f54d
RustyNail <takonoyawarakaage@yahoo.co.jp>
2018-02-20 17:04:23 +0900

    チュートリアルに不要なコードを削除

--- spec/pages/reservation_completed.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'layout'
-
-class ReservationCompleted < Layout
-  set_url '/reserveApp_Renewal/final_confirm.html{?query*}'
-
-  element :back_button, '#returnto_checkInfo'
-  element :message, '#slide1'
-end
---
+++ spec/reservation_spec.rb
@@ -0,0 +1,4 @@
+require_relative 'spec_helper'
+
+RSpec.describe 'Reservation feature' do
+end
